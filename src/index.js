5b18ed3918efccb004c019740465b3a508f35d95
Shirtiny <shirtiny@gmail.com>
2020-06-21 16:27:08 +0800

    一星期的努力 ffmpeg.js 携带libass fix字体的版本终于编译成功！！浏览器内视频渲染，字幕内封测试通过！！

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import "font-awesome/css/font-awesome.css";
 import "nprogress/nprogress.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-virtualized/styles.css";
+import fileService from "./services/fileService";
+import subService from "./services/subService";
 
 const isMobile = guideService.isMobile();
 
@@ -26,6 +28,60 @@ ReactDOM.render(
   ),
   document.getElementById("root")
 );
+
+const worker = new Worker("/ffmpeg-worker-mp4.js");
+
+worker.onmessage = function (e) {
+  const msg = e.data;
+  switch (msg.type) {
+    case "ready":
+      console.log("shFFmpegWorker ready");
+      break;
+    case "stdout":
+      console.log(msg.data);
+      break;
+    case "stderr":
+      console.log(msg.data);
+      break;
+    case "done":
+      console.log("shFFmpegWorker works done", msg.data);
+      const res = msg.data.MEMFS[0];
+      const { name, data } = res;
+      const url = URL.createObjectURL(
+        new File([data.buffer], name, { type: "video/mp4" })
+      );
+      subService.downloadFromUrl(url, name);
+      break;
+    default:
+      return;
+  }
+};
+const testFfmpeg = async () => {
+  const testData = await fileService.fetchFileData("/friday.mp4");
+  const vttData = await fileService.fetchFileData("/friday.vtt");
+  const ttfData = await fileService.fetchFileData("/default.ttf");
+  console.log(testData, vttData);
+  worker.postMessage({
+    type: "run",
+    TOTAL_MEMORY: 256 * 1024 * 1024,
+    arguments: ["-y","-i","test.vtt","output.ass"],
+    // arguments: [
+    //   "-y",
+    //   "-i",
+    //   "test.mp4",
+    //   "-vf",
+    //   "subtitles=test.vtt",
+    //   "output.mp4",
+    // ],
+    MEMFS: [
+      { name: "test.mp4", data: testData },
+      { name: "test.vtt", data: vttData },
+      { name: "default.ttf", data: ttfData },
+    ],
+  });
+};
+
+// testFfmpeg();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/services/fileService.js
@@ -0,0 +1,118 @@
+//PromiseExecutor 将文件读取为ArrayBuffer
+function readFilePE(resolve, reject, file, readerFnName) {
+  const reader = new FileReader();
+  // reader.readAsArrayBuffer(file);
+  // reader.
+  reader[readerFnName](file);
+  //出错时
+  reader.onerror = (e) => {
+    reject(e);
+  };
+  //读取完成时 成功/失败
+  reader.onload = () => {
+    //失败时 rs为null
+    resolve(reader.result);
+  };
+}
+
+export async function readFileData(file) {
+  let buffer = 0;
+  try {
+    buffer = await new Promise((resolve, reject) =>
+    readFilePE(resolve, reject, file, "readAsArrayBuffer")
+    );
+  } catch (e) {
+    console.log("文件读取失败", e);
+  }
+  return new Uint8Array(buffer);
+}
+
+export async function fetchAsBlob(url) {
+  const res = await fetch(url);
+  return await res.blob();
+}
+
+export async function fetchFileData(url) {
+  const blob = await fetchAsBlob(url);
+  console.log("fetched blob:", blob);
+  return await readFileData(blob);
+}
+
+//合并 比如：传入两个unit8数组
+export function merge(...buffers) {
+  //获取第一个unit数组的构造函数
+  const Constructor = buffers[0].constructor;
+  return buffers.reduce((pre, val) => {
+    //新建一个数组 长度为两个数组的长度和
+    const merge = new Constructor((pre.byteLength | 0) + (val.byteLength | 0));
+    merge.set(pre, 0);
+    merge.set(val, pre.byteLength | 0);
+    return merge;
+  }, new Constructor());
+}
+
+function fetchFileU8DataPE(resove, reject, url) {
+  let abortController = new AbortController();
+  let reader = null;
+  fetch(url, { signal: abortController.signal }).then((res) => {
+    //如果res.body是流
+    if (res.body && typeof res.body.getReader === "function") {
+      //获取 ReadableStream 的 reader , ReadableStream接口呈现了一个可读取的二进制流操作 Fetch 通过Response的属性 body提供了一个具体的 ReadableStream 对象。
+      reader = res.body.getReader();
+      // 8位无符号整型数组 Uint8Array与可读流ReadableStream 的协同用法。
+      let data = new Uint8Array();
+      //使用reader读取二进制数据  "done"是一个布尔型，"value"是一个Unit8Array
+      reader.read().then(function read({ done, value }) {
+        if (done) {
+          abortController.abort();
+          reader.cancel();
+          abortController = null;
+          reader = null;
+          resove(data);
+          return;
+        }
+        //合并两个unit8数组 (累加data
+        data = merge(data, value);
+        //递归
+        return reader.read().then(read);
+      });
+    } else {
+      reject("res.body不是流");
+    }
+  });
+}
+
+export async function fetchFileU8Data(url) {
+  let data = new Uint8Array();
+  try {
+    data = await new Promise((resolve, reject) =>
+      fetchFileU8DataPE(resolve, reject, url)
+    );
+  } catch (e) {
+    console.log("文件读取失败", e);
+  }
+  return data;
+}
+
+export async function fetchFileAsText(url) {
+  const blob = await fetchAsBlob(url);
+  console.log("fetched blob:", blob);
+  let text = "";
+  try {
+    text = await new Promise((resolve, reject) =>
+    readFilePE(resolve, reject, blob, "readAsText")
+    );
+  } catch (e) {
+    console.log("文件读取失败", e);
+  }
+  return text;
+}
+
+const fileService = {
+  readFileData,
+  fetchFileData,
+  fetchFileAsText,
+  fetchFileU8Data,
+};
+
+export default fileService;
