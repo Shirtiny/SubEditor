47c40a7282e945840b9bec60913e72d7e7823c1a
shirtiny <shirtiny@gmail.com>
2020-07-07 09:09:41 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2fd38f0df1b4378001bb831eae77b062.js"
+  "./precache-manifest.565822e41277075094acbcf3137a59ad.js"
 );
 
 self.addEventListener('message', (event) => {
