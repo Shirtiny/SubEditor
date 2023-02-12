fd09a68410123c11068594751c5d048b87568468
majh <majh@cunxin.com>
2023-02-12 23:50:25 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ed7266212b2ce9b9e6ac6f511a200f41.js"
+  "./precache-manifest.8bc1eb0bb0c94f27a623ce77d79694a2.js"
 );
 
 self.addEventListener('message', (event) => {
